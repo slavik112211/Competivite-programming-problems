5ebe79e7b09b1ee33cdbd5344a9af12792341155
Slavik Derevyanko <slavik2121@gmail.com>
2016-02-06 11:25:07 -0500

    Formatting: changed tabs to spaces for IntCharSorter

--- 15 LinkedIn/IntCharSorterTest.java
+++ 15 LinkedIn/IntCharSorterTest.java
@@ -8,117 +8,117 @@ import competivite_programming_problems.IntCharSorter;
 import competivite_programming_problems.IntCharSorter.Element;
 
 public class IntCharSorterTest {
-	
-	ArrayList<Element> listA = new ArrayList<Element>();
-	ArrayList<Element> listB = new ArrayList<Element>();
-	ArrayList<Element> listC = new ArrayList<Element>();
-	ArrayList<Element> result = new ArrayList<Element>();
-	ArrayList<Element> sampleElements = new ArrayList<Element>();
 
-	public IntCharSorterTest(){
-		Element[] elems = new Element[]{
-			new Element('a'), new Element('b'), new Element('c'), new Element('d'),
-			new Element(1), new Element(2), new Element(3), new Element(4), new Element(8)};
-		sampleElements=new ArrayList<Element>(Arrays.asList(elems));
-	}
-	
-	@Before
-	public void setUp(){
-		listA.clear();
-		listB.clear();
-		listC.clear();
-		result.clear();
-	}
+    ArrayList<Element> listA = new ArrayList<Element>();
+    ArrayList<Element> listB = new ArrayList<Element>();
+    ArrayList<Element> listC = new ArrayList<Element>();
+    ArrayList<Element> result = new ArrayList<Element>();
+    ArrayList<Element> sampleElements = new ArrayList<Element>();
 
-   @Test
-   public void shouldMergeEmptyArrays() {
-	  listC = IntCharSorter.merge(listA, listB);
-	  result = new ArrayList<Element>();
-      assertEquals(listC,result);
-   }
-   
-   @Test
-   public void shouldMergeOneElementArrayWithEmptyArray() {
-	  listA.add(sampleElements.get(0));
-	  listC = IntCharSorter.merge(listA, listB);
-	  result.add(sampleElements.get(0));
-      assertEquals(listC,result);
-   }
-   
-   @Test
-   public void shouldMergeOneElementArrays1() { //number,number
-	  listA.add(sampleElements.get(6));
-	  listB.add(sampleElements.get(4));
-	  listC = IntCharSorter.merge(listA, listB);
-	  result.add(sampleElements.get(4));
-	  result.add(sampleElements.get(6));
-      assertEquals(listC,result);
-   }
-   
-   @Test
-   public void shouldMergeOneElementArrays2() { //char,number
-	  listA.add(sampleElements.get(6));
-	  listB.add(sampleElements.get(2));
-	  listC = IntCharSorter.merge(listA, listB);
-	  result.add(sampleElements.get(6));
-	  result.add(sampleElements.get(2));
-      assertEquals(listC,result);
-   }
-   
-   @Test
-   public void shouldMergeOneElementArrays3() { //number,char
-	  listA.add(sampleElements.get(6));
-	  listB.add(sampleElements.get(1));
-	  listC = IntCharSorter.merge(listA, listB);
-	  result.add(sampleElements.get(6));
-	  result.add(sampleElements.get(1));
-      assertEquals(listC,result);
-   }
-   
-   @Test
-   public void shouldMergeOneElementArrays4() { //char,char
-	  listA.add(sampleElements.get(3));
-	  listB.add(sampleElements.get(2));
-	  listC = IntCharSorter.merge(listA, listB);
-	  result.add(sampleElements.get(2));
-	  result.add(sampleElements.get(3));
-      assertEquals(listC,result);
-   }
+    public IntCharSorterTest(){
+        Element[] elems = new Element[]{
+            new Element('a'), new Element('b'), new Element('c'), new Element('d'),
+            new Element(1), new Element(2), new Element(3), new Element(4), new Element(8)};
+        sampleElements=new ArrayList<Element>(Arrays.asList(elems));
+    }
 
-   @Test
-   public void shouldMergeEvenArrays() {
-	Element[] elems1 = new Element[]{
-		new Element('b'), new Element(4), new Element('c'), new Element(8),new Element('d')};
-	listA=new ArrayList<Element>(Arrays.asList(elems1));
+    @Before
+    public void setUp(){
+        listA.clear();
+        listB.clear();
+        listC.clear();
+        result.clear();
+    }
 
-	Element[] elems2 = new Element[]{
-		new Element(2), new Element('a'), new Element(3), new Element(4),new Element('c')};
-	listB=new ArrayList<Element>(Arrays.asList(elems2));
-	  
-	Element[] elems3 = new Element[]{
-		new Element('a'), new Element(2), new Element('b'),new Element(3), new Element('c'),
-		new Element(4), new Element('c'), new Element(4), new Element(8), new Element('d')};
-	result=new ArrayList<Element>(Arrays.asList(elems3));
+    @Test
+    public void shouldMergeEmptyArrays() {
+        listC = IntCharSorter.merge(listA, listB);
+        result = new ArrayList<Element>();
+        assertEquals(listC,result);
+    }
 
-	listC = IntCharSorter.merge(listA, listB);
-	assertEquals(listC,result);
-   }
+    @Test
+    public void shouldMergeOneElementArrayWithEmptyArray() {
+        listA.add(sampleElements.get(0));
+        listC = IntCharSorter.merge(listA, listB);
+        result.add(sampleElements.get(0));
+        assertEquals(listC,result);
+    }
 
-   @Test
-   public void shouldMergeUnevenArrays() {
-	Element[] elems1 = new Element[]{
-		new Element('b'), new Element(4), new Element('c'), new Element(8), new Element('d'), new Element(9)};
-	listA=new ArrayList<Element>(Arrays.asList(elems1));
+    @Test
+    public void shouldMergeOneElementArrays1() { //number,number
+        listA.add(sampleElements.get(6));
+        listB.add(sampleElements.get(4));
+        listC = IntCharSorter.merge(listA, listB);
+        result.add(sampleElements.get(4));
+        result.add(sampleElements.get(6));
+        assertEquals(listC,result);
+    }
 
-	Element[] elems2 = new Element[]{
-		new Element(2), new Element('a'), new Element(3), new Element(4),new Element('c')};
-	listB=new ArrayList<Element>(Arrays.asList(elems2));
-	  
-	Element[] elems3 = new Element[]{
-		new Element('a'), new Element(2), new Element('b'),new Element(3), new Element('c'), new Element(4),
-		new Element(4), new Element('c'), new Element(8), new Element(9), new Element('d')};
-	result=new ArrayList<Element>(Arrays.asList(elems3));
-	listC = IntCharSorter.merge(listA, listB);
-	assertEquals(listC,result);
-   }
+    @Test
+    public void shouldMergeOneElementArrays2() { //char,number
+        listA.add(sampleElements.get(6));
+        listB.add(sampleElements.get(2));
+        listC = IntCharSorter.merge(listA, listB);
+        result.add(sampleElements.get(6));
+        result.add(sampleElements.get(2));
+        assertEquals(listC,result);
+    }
+
+    @Test
+    public void shouldMergeOneElementArrays3() { //number,char
+        listA.add(sampleElements.get(6));
+        listB.add(sampleElements.get(1));
+        listC = IntCharSorter.merge(listA, listB);
+        result.add(sampleElements.get(6));
+        result.add(sampleElements.get(1));
+        assertEquals(listC,result);
+    }
+
+    @Test
+    public void shouldMergeOneElementArrays4() { //char,char
+        listA.add(sampleElements.get(3));
+        listB.add(sampleElements.get(2));
+        listC = IntCharSorter.merge(listA, listB);
+        result.add(sampleElements.get(2));
+        result.add(sampleElements.get(3));
+        assertEquals(listC,result);
+    }
+
+    @Test
+    public void shouldMergeEvenArrays() {
+        Element[] elems1 = new Element[]{
+            new Element('b'), new Element(4), new Element('c'), new Element(8),new Element('d')};
+        listA=new ArrayList<Element>(Arrays.asList(elems1));
+
+        Element[] elems2 = new Element[]{
+            new Element(2), new Element('a'), new Element(3), new Element(4),new Element('c')};
+        listB=new ArrayList<Element>(Arrays.asList(elems2));
+
+        Element[] elems3 = new Element[]{
+            new Element('a'), new Element(2), new Element('b'),new Element(3), new Element('c'),
+            new Element(4), new Element('c'), new Element(4), new Element(8), new Element('d')};
+        result=new ArrayList<Element>(Arrays.asList(elems3));
+
+        listC = IntCharSorter.merge(listA, listB);
+        assertEquals(listC,result);
+    }
+
+    @Test
+    public void shouldMergeUnevenArrays() {
+        Element[] elems1 = new Element[]{
+            new Element('b'), new Element(4), new Element('c'), new Element(8), new Element('d'), new Element(9)};
+        listA=new ArrayList<Element>(Arrays.asList(elems1));
+
+        Element[] elems2 = new Element[]{
+            new Element(2), new Element('a'), new Element(3), new Element(4),new Element('c')};
+        listB=new ArrayList<Element>(Arrays.asList(elems2));
+
+        Element[] elems3 = new Element[]{
+            new Element('a'), new Element(2), new Element('b'),new Element(3), new Element('c'), new Element(4),
+            new Element(4), new Element('c'), new Element(8), new Element(9), new Element('d')};
+        result=new ArrayList<Element>(Arrays.asList(elems3));
+        listC = IntCharSorter.merge(listA, listB);
+        assertEquals(listC,result);
+    }
 }
--- src/competivite_programming_problems/IntCharSorter.java
+++ src/competivite_programming_problems/IntCharSorter.java
@@ -4,127 +4,127 @@ import java.util.ArrayList;
 
 public class IntCharSorter
 {
-	public static class Element {
-		public int number;
-		public char character;
-		public Boolean isNumber=false, isCharacter=false;
-		public Element(int number){
-			this.number = number;
-			isNumber=true;
-		}
-		public Element(char character){
-			this.character = character;
-			isCharacter=true;
-		}
-		
-		public boolean isComparable(Element other){
-			return (this.isNumber && other.isNumber) ||
-					(this.isCharacter && other.isCharacter) ? true : false;
-		}
-		
-		public String toString(){
-			return isNumber ? Integer.toString(number) : ""+character;
-		}
-		
-		public boolean equals(Object other){
-			Element otherEl = (Element) other;
-			return ((this.isNumber && otherEl.isNumber && this.number==otherEl.number) ||
-					(this.isCharacter && otherEl.isCharacter && this.character==otherEl.character)) ? true : false;
-		}
-	}
-	
-  public static ArrayList<Element> merge(ArrayList<Element> listA, ArrayList<Element> listB){
-	  int totalSize = listA.size()+listB.size();
-	  int i, resultArrayIndex=0;
-	  Element leftEl1, leftEl2, rightEl1, rightEl2;
-	  ArrayList<Element> result = new ArrayList<Element>(totalSize);
-	  boolean[] intPositions=new boolean[totalSize];
-	  int iteratorLeft1=0, iteratorLeft2=0, iteratorRight1=0, iteratorRight2=0;
-	  
-	  //1. Determine positions of int/char
-	  for(i=0; i<listA.size();i++){
-		  intPositions[resultArrayIndex] = listA.get(i).isNumber ? true : false;
-		  resultArrayIndex++;
-	  }
-	  for(i=0; i<listB.size();i++){
-		  intPositions[resultArrayIndex] = listB.get(i).isNumber ? true : false;
-		  resultArrayIndex++;
-	  }
-	  
-	  resultArrayIndex=0;
-	  while(resultArrayIndex<totalSize){
-		  leftEl1  = (iteratorLeft1<listA.size())  ? listA.get(iteratorLeft1)  : null;
-		  leftEl2  = (iteratorLeft2<listA.size())  ? listA.get(iteratorLeft2)  : null;
-		  rightEl1 = (iteratorRight1<listB.size()) ? listB.get(iteratorRight1) : null;
-		  rightEl2 = (iteratorRight2<listB.size()) ? listB.get(iteratorRight2) : null;
-		  
-		  //1. Current position in result[] requires a number
-		  if(intPositions[resultArrayIndex]){
-			  if((leftEl1==null    && rightEl1.isNumber) || 
-				 (leftEl1.isNumber && rightEl1==null)    ||
-				 (leftEl1.isNumber && rightEl1.isNumber)){
-				  if(rightEl1==null || leftEl1.number<=rightEl1.number){
-					  result.add(leftEl1);
-					  iteratorLeft1++;
-				  }else{
-					  result.add(rightEl1);
-					  iteratorRight1++;
-				  }
-				  resultArrayIndex++;
-			  }else if((leftEl1==null || leftEl1.isNumber) && rightEl1.isCharacter){
-				  iteratorRight1++;
-			  }else if(leftEl1.isCharacter && (rightEl1==null || rightEl1.isNumber)){
-				  iteratorLeft1++;
-			  }else if(leftEl1.isCharacter && rightEl1.isCharacter){
-				  iteratorLeft1++;
-				  iteratorRight1++;
-			  }
-		  }
-		  else{ //2. Current position in result[] requires a character
-			  if((leftEl2.isCharacter && rightEl2 == null) ||
-				 (leftEl2 == null     && rightEl2.isCharacter) ||
-				 (leftEl2.isCharacter && rightEl2.isCharacter)){
-				  if(rightEl2==null || leftEl2.character<=rightEl2.character){
-					  result.add(leftEl2);
-					  iteratorLeft2++;
-				  }else{
-					  result.add(rightEl2);
-					  iteratorRight2++;
-				  }
-				  resultArrayIndex++;
-			  }else if(leftEl2.isNumber && (rightEl2==null || rightEl2.isCharacter)){
-				  iteratorLeft2++;
-			  }else if((leftEl2==null || leftEl2.isCharacter) && rightEl2.isNumber){
-				  iteratorRight2++;
-			  }else if(leftEl2.isNumber && rightEl2.isNumber){
-				  iteratorLeft2++;
-				  iteratorRight2++;
-			  }
-		  }
-	  }
-	  return result;
-  }
+    public static class Element {
+        public int number;
+        public char character;
+        public Boolean isNumber=false, isCharacter=false;
+        public Element(int number){
+            this.number = number;
+            isNumber=true;
+        }
+        public Element(char character){
+            this.character = character;
+            isCharacter=true;
+        }
 
-  
-  public static void main(String[] args){
-	  ArrayList<Element> listA = new ArrayList<Element>();
-	  listA.add(new Element('b'));
-	  listA.add(new Element(4));
-	  listA.add(new Element('c'));
-	  listA.add(new Element(8));
-	  listA.add(new Element('d'));
-	  listA.add(new Element(9));
-	  
-	  ArrayList<Element> listB = new ArrayList<Element>();
-	  listB.add(new Element(2));
-	  listB.add(new Element('a'));
-	  listB.add(new Element(3));
-	  listB.add(new Element(4));
-	  listB.add(new Element('c'));
-	  
-	  ArrayList<Element> listC = IntCharSorter.merge(listA, listB);
-	  System.out.println(listC);
-	  //[a, 2, b, 3, c, 4, 4, c, 8, 9, d]
-	  
-  }
+        public boolean isComparable(Element other){
+            return (this.isNumber && other.isNumber) ||
+                    (this.isCharacter && other.isCharacter) ? true : false;
+        }
+
+        public String toString(){
+            return isNumber ? Integer.toString(number) : ""+character;
+        }
+
+        public boolean equals(Object other){
+            Element otherEl = (Element) other;
+            return ((this.isNumber && otherEl.isNumber && this.number==otherEl.number) ||
+                    (this.isCharacter && otherEl.isCharacter && this.character==otherEl.character)) ? true : false;
+        }
+    }
+
+    public static ArrayList<Element> merge(ArrayList<Element> listA, ArrayList<Element> listB){
+        int totalSize = listA.size()+listB.size();
+        int i, resultArrayIndex=0;
+        Element leftEl1, leftEl2, rightEl1, rightEl2;
+        ArrayList<Element> result = new ArrayList<Element>(totalSize);
+        boolean[] intPositions=new boolean[totalSize];
+        int iteratorLeft1=0, iteratorLeft2=0, iteratorRight1=0, iteratorRight2=0;
+
+        //1. Determine positions of int/char
+        for(i=0; i<listA.size();i++){
+            intPositions[resultArrayIndex] = listA.get(i).isNumber ? true : false;
+            resultArrayIndex++;
+        }
+        for(i=0; i<listB.size();i++){
+            intPositions[resultArrayIndex] = listB.get(i).isNumber ? true : false;
+            resultArrayIndex++;
+        }
+
+        resultArrayIndex=0;
+        while(resultArrayIndex<totalSize){
+            leftEl1  = (iteratorLeft1<listA.size())  ? listA.get(iteratorLeft1)  : null;
+            leftEl2  = (iteratorLeft2<listA.size())  ? listA.get(iteratorLeft2)  : null;
+            rightEl1 = (iteratorRight1<listB.size()) ? listB.get(iteratorRight1) : null;
+            rightEl2 = (iteratorRight2<listB.size()) ? listB.get(iteratorRight2) : null;
+
+            //1. Current position in result[] requires a number
+            if(intPositions[resultArrayIndex]){
+                if((leftEl1==null    && rightEl1.isNumber) || 
+                   (leftEl1.isNumber && rightEl1==null)    ||
+                   (leftEl1.isNumber && rightEl1.isNumber)){
+                    if(rightEl1==null || leftEl1.number<=rightEl1.number){
+                        result.add(leftEl1);
+                        iteratorLeft1++;
+                    }else{
+                        result.add(rightEl1);
+                        iteratorRight1++;
+                    }
+                    resultArrayIndex++;
+                }else if((leftEl1==null || leftEl1.isNumber) && rightEl1.isCharacter){
+                    iteratorRight1++;
+                }else if(leftEl1.isCharacter && (rightEl1==null || rightEl1.isNumber)){
+                    iteratorLeft1++;
+                }else if(leftEl1.isCharacter && rightEl1.isCharacter){
+                    iteratorLeft1++;
+                    iteratorRight1++;
+                }
+            }
+            else{ //2. Current position in result[] requires a character
+                if((leftEl2.isCharacter && rightEl2 == null) ||
+                   (leftEl2 == null     && rightEl2.isCharacter) ||
+                   (leftEl2.isCharacter && rightEl2.isCharacter)){
+                    if(rightEl2==null || leftEl2.character<=rightEl2.character){
+                        result.add(leftEl2);
+                        iteratorLeft2++;
+                    }else{
+                        result.add(rightEl2);
+                        iteratorRight2++;
+                    }
+                    resultArrayIndex++;
+                }else if(leftEl2.isNumber && (rightEl2==null || rightEl2.isCharacter)){
+                    iteratorLeft2++;
+                }else if((leftEl2==null || leftEl2.isCharacter) && rightEl2.isNumber){
+                    iteratorRight2++;
+                }else if(leftEl2.isNumber && rightEl2.isNumber){
+                    iteratorLeft2++;
+                    iteratorRight2++;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args){
+        ArrayList<Element> listA = new ArrayList<Element>();
+        listA.add(new Element('b'));
+        listA.add(new Element(4));
+        listA.add(new Element('c'));
+        listA.add(new Element(8));
+        listA.add(new Element('d'));
+        listA.add(new Element(9));
+
+        ArrayList<Element> listB = new ArrayList<Element>();
+        listB.add(new Element(2));
+        listB.add(new Element('a'));
+        listB.add(new Element(3));
+        listB.add(new Element(4));
+        listB.add(new Element('c'));
+
+        ArrayList<Element> listC = IntCharSorter.merge(listA, listB);
+        System.out.println(listC);
+        //[a, 2, b, 3, c, 4, 4, c, 8, 9, d]
+
+    }
 }
